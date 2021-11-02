e1e454e83057aa00def6e8c902f5962bf83a3add
Chaima <chaima-aicheche@laplateforme.io>
2021-11-02 17:31:28 +0100

    variable str echo et bool

---
+++ jour01/job01/index.php
@@ -0,0 +1,23 @@
+<?php
+echo 'Hello LaPlateforme!';
+$str='LaPlateforme';
+$str2='Vive';
+$str3='!';
+echo "<br/>".$str2." ".$str." " .$str3; 
+$val=6;
+echo "<br/>".$val."<br/>"; 
+echo $val + 4;
+$myBool= true;
+echo "<br/>".$myBool;
+$myBool= false;
+echo "<br/>".$myBool;
+
+
+
+
+
+
+
+
+
+?>
