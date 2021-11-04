0d7188ec5db1068958489143ed988600cc524698
Chaima <chaima-aicheche@laplateforme.io>
2021-11-04 22:28:08 +0100

    job1

---
+++ jour03/job01/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    $tab = [200, 204, 173, 98, 171, 404, 459];
+
+for($i = 0; $i < 7; $i++){
+    if($tab[$i] % 2 == 1)
+    {
+        echo $tab[$i].' est impair !'.'<br/>';
+    }
+   else
+
+    {
+        echo $tab[$i].' est pair !'.'<br/>';
+    
+    }
+}
+ 
+?>
