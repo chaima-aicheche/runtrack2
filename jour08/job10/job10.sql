c94c87daac94f1257392c05c09cec7067e867219
Chaima <chaima-aicheche@laplateforme.io>
2021-11-10 21:42:43 +0100

    jour08

---
+++ jour08/job10/job10.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Nov 10, 2021 at 05:15 PM
+-- Server version: 5.7.24
+-- PHP Version: 7.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `jour08`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `etage`
+--
+
+CREATE TABLE `etage` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `superficie` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `etage`
+--
+
+INSERT INTO `etage` (`id`, `nom`, `numero`, `superficie`) VALUES
+(1, 'RDC', 0, 500),
+(2, 'R+1', 1, 500);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `etage`
+--
+ALTER TABLE `etage`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
