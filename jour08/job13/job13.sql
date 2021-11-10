38a3ab33c2ef86e45afdeae896f91ed08422729d
Chaima <chaima-aicheche@laplateforme.io>
2021-11-10 22:31:24 +0100

    JOB13

---
+++ jour08/job13/job13.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Nov 10, 2021 at 09:30 PM
+-- Server version: 5.7.24
+-- PHP Version: 7.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `jour08`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `salles`
+--
+
+CREATE TABLE `salles` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `id_etage` int(11) NOT NULL,
+  `capacite` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `salles`
+--
+
+INSERT INTO `salles` (`id`, `nom`, `id_etage`, `capacite`) VALUES
+(1, 'Lounge', 1, 100),
+(2, 'Studio Son', 1, 5),
+(3, 'Broadcasting', 2, 50),
+(4, 'Bocal Peda', 2, 4),
+(5, 'Coworking', 2, 80),
+(6, 'Studio Video', 2, 5);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
