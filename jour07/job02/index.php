b89f35ca24edb93952193c440a27b4824c034990
Chaima <chaima-aicheche@laplateforme.io>
2021-11-14 15:09:00 +0100

    jour 7 job 1 2

---
+++ jour07/job02/index.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+?>
+
+<form action="index.php" method="get">
+
+<?php
+setcookie(['nbvisites']);
+
+if (isset($_SESSION['nbvisites']))
+{
+    $_SESSION['nbvisites']++;
+} 
+
+else
+ {
+    $_SESSION['nbvisites'] = 0;
+}
+
+echo 'Visites : ' . $_SESSION['nbvisites'];
+
+if (isset($_GET['reset'])) 
+{
+    unset($_SESSION['nbvisites']);
+}
+
+?>
+
+<input type="submit" name="reset" value="reset">
+</form>
